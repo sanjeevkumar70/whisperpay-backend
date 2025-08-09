9bb65ba5fffa847372ea95128c67dcda4733d971
SANJEEV KUMAR <59915362+sanjeevkumar70@users.noreply.github.com>
2025-08-10 01:31:33 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME||"web", process.env.DB_USER||"root", process.env.DB_PASSWORD||"root", {
   host: process.env.DB_HOST,
   dialect: "mysql",
   logging: false, // set to console.log to debug
